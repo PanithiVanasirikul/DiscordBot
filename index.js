e3182edaf33f1e0f237cc6f827f140553e62a0ee
Panithi Vanasirikul <scher3141@gmail.com>
2022-01-14 23:05:29 +0700

    added event handler

--- index.js
+++ index.js
@@ -1,26 +1,48 @@
-const { Client, Intents} = require('discord.js');
+const fs = require('fs');
+const { Client, Intents, Collection} = require('discord.js');
 const {token} = require('./config.json');
 
 const client = new Client({intents: [Intents.FLAGS.GUILDS]});
 
-client.once('ready', () =>{
-    console.log('Ready!');
-});
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-client.on('interactionCreate', async interaction =>{
-    if(!interaction.isCommand()) return;
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
 
-    const {commandName} = interaction;
-    if(commandName ==='ping'){
-        await interaction.reply('Pong!')
-    } else if (commandName === 'server') {
-		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-	} else if (commandName === 'user') {
-		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
-	}
-    
-});
+const eventFiles = fs.readdirSync('./events/').filter(file => file.endsWith('.js'));
 
+for(const file of eventFiles){
+    const event = require(`./events/${file}`);
+    if(event.once){
+        client.once(event.name, (...args) => event.execute(...args));
+    }
+    else{
+        client.on(event.name, (...args) => event.execute(client,...args));
+    }
+}
+
+
+// client.once('ready', () => {
+// 	console.log('Ready!');
+// });
+
+// client.on('interactionCreate', async interaction => {
+// 	if (!interaction.isCommand()) return;
+
+// 	const command = client.commands.get(interaction.commandName);
+
+// 	if (!command) return;
+
+// 	try {
+// 		await command.execute(interaction);
+// 	} catch (error) {
+// 		console.error(error);
+// 		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+// 	}
+// });
 
 
 client.login(token);
