81091a67bf34ee4fd82ec18243b7f0da01657f85
Panithi Vanasirikul <scher3141@gmail.com>
2022-01-17 03:48:43 +0700

    added yt links(guild_voice_states not done)

--- commands/play.js
+++ commands/play.js
@@ -1,7 +1,14 @@
 const ytdl = require('ytdl-core');
 const ytSearch = require('yt-search');
-const {joinVoiceChannel} = require('@discordjs/voice');
 const {SlashCommandBuilder} = require('@discordjs/builders');
+const { VoiceChannel } = require('discord.js');
+const {
+	AudioPlayerStatus,
+	StreamType,
+	createAudioPlayer,
+	createAudioResource,
+	joinVoiceChannel,
+} = require('@discordjs/voice');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -9,25 +16,23 @@ module.exports = {
         .setDescription('Joins and Play a video from yt')
         .addStringOption(option => option.setName('link').setDescription('URL, query')),
     async execute(client, interaction){
-        const channel = await client.channels.fetch("884404283175473163");
+        const channel = await interaction.member.voice.channel;
         // console.log(channel.guild);
-        
         const connection = joinVoiceChannel({
-            channelId: "884404283175473163",
-            guildId: '824969885263331358',
+            channelId: channel.id,
+            guildId: channel.guild.id,
             adapterCreator: channel.guild.voiceAdapterCreator,
         });
-        await interaction.reply('fuck you');
-        // await console.log(user);
-        // const vc = client.channels.fetch("884404283175473163");
         // if(!vc) return await interaction.reply('You need to be in a channel to execute this command.');
-        // const connection = await vc.join();
+        console.log(interaction.options.getString('link'));
+        const stream = ytdl(interaction.options.getString('link'), {filter: 'audioonly'});
+        const resource = createAudioResource(stream, {inputType: StreamType.Arbitrary});
+        const player = createAudioPlayer();
 
-        // const vidFinder = async(query) => {
-        //     const vidResult = await ytSearch(query);
+        player.play(resource);
+        connection.subscribe(player);
 
-        //     if(vidResult.videos.length > 1) return vidResult.videos[0];
-        //     return null;
-        // }
+        player.on(AudioPlayerStatus.Idle, () => connection.destroy());
+
     },
 };
