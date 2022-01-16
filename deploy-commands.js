5badd6a69980b002f10d7941c1e738463da91cc3
Panithi Vanasirikul <scher3141@gmail.com>
2022-01-16 16:38:18 +0700

    trying out registering slash commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -13,6 +13,17 @@ for (const file of commandFiles){
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+// rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+// 	.then(() => console.log('Successfully registered application commands.'))
+// 	.catch(console.error);
+
+(async () => {
+    try{
+        console.log("refreshing Slash commands.");
+
+        await rest.put(Routes.applicationCommands(clientId), {body: commands},);
+        console.log('Successfully reloaded Slash commands');
+    } catch(error){
+        console.error(error);
+    }
+})();
